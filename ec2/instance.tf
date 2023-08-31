28a3f3f4e1d8ac8351f1ac7d04c61cde3e33b1a4
JorgeVS <jvazquez@forrester.com>
2023-08-30 20:52:32 -0400

    Creating ec2 instance

---
+++ ec2/instance.tf
@@ -0,0 +1,4 @@
+resource "aws_instance" "jorgevs-server" {
+  ami = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+}
---
+++ ec2/variables.tf
@@ -0,0 +1,66 @@
+/*variable "name" {
+  type = string
+}
+
+variable "stage" {
+  type = string
+}
+
+variable "type" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "endpoint_id" {
+  type = string
+  default = ""
+}
+
+variable "domain_name" {
+  type = string
+}
+
+variable "cloudwatch_log_retention" {
+  type = number
+  default = 7
+}
+
+variable "eks_vpc_link_id" {
+  type = string
+}
+
+variable "eks_lb_arn" {
+  type = string
+}
+
+variable "prov_vpc_link_id" {
+  type = string
+}
+
+variable "prov_lb_arn" {
+  type = string
+}
+
+variable "usage_plans" {
+  type = any
+}
+
+variable "usage_plans_v2" {
+  type = any
+}
+
+variable "usage_plan_preffix" {
+  type = string
+}
+
+variable "certificate_domain_name" {
+  type = string
+  default = "*.forrester.com"
+}
+
+variable "services_domain_name_map" {
+  type = string
+}*/
